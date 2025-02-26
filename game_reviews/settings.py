90590ca6b5ed238a3e10a15f25e43ab2aa0d1253
Johan-4P <3db9470n@students.codeinstitute.net>
2025-02-26 17:57:50 +0100

    add secret_key

--- game_reviews/settings.py
+++ game_reviews/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w(v)i696d!ht2^m=2f*nw&*8c!=o1r)g!umibg&f65c%i*mw68'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
