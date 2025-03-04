2fea363bd726956118b9b9c8a37e04246859b9d0
Johan-4P <3db9470n@students.codeinstitute.net>
2025-03-04 10:44:29 +0100

    Set DEBUG to True for development

--- reviews/urls.py
+++ reviews/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('<slug:slug>/comment/', views.add_comment, name='add_comment'),
     path('comment/<int:comment_id>/edit/', views.edit_comment, name='edit_comment'),
     path('comment/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
+    
 ]
