eaa0fd7afdb85c7c291bfc84462e3698891b01a5
Johan-4P <3db9470n@students.codeinstitute.net>
2025-03-06 16:48:48 +0100

    Enhance user games view to include comment count for each game

--- user_games/views.py
+++ user_games/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from reviews.models import Game  
-from .forms import UserGameForm  
-from reviews.views import GameList 
+from reviews.models import Game
+from .forms import UserGameForm
+from django.db.models import Count
 
 @login_required
 def user_games(request):
-
-    games = Game.objects.filter(author=request.user)
+    games = Game.objects.filter(author=request.user).annotate(comment_count=Count('comments'))
     return render(request, 'user_games/user_games.html', {'games': games})
 
 @login_required
