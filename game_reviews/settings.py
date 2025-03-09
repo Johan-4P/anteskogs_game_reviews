6effee773a283e44006d6abb2df45ab6c726aa87
Johan-4P <3db9470n@students.codeinstitute.net>
2025-03-09 12:14:00 +0100

    Disable debug mode and enhance comment management with edit and delete options

--- game_reviews/settings.py
+++ game_reviews/settings.py
@@ -33,7 +33,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['anteskogs-game-reviews-c55343667d70.herokuapp.com', 'localhost', '127.0.0.1']
 
